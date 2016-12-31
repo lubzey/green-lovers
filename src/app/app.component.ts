9e5646e1453902af0b5f2d2ee02c9070f18e4944
lubzey <lubzdudz@gmail.com>
2016-12-31 13:55:54 +0200

    Small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 import { DataService } from './shared/services/data.service';
 
