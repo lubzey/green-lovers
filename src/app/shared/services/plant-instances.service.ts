f5ab201e6cb54d7c4f640b679eba6072a503e44b
lubzey <lubzdudz@gmail.com>
2016-12-31 18:49:34 +0200

    fixed seed

--- src/app/shared/services/plant-instances.service.ts
+++ src/app/shared/services/plant-instances.service.ts
@@ -8,6 +8,7 @@ export class PlantInstancesService {
   plantInstances: FirebaseListObservable<any[]>;
 
   constructor(private af: AngularFire) {
+    const promise = af.database.list('/plant-instances').remove();
     this.seedPlant = af.database.object('/greenloversclub');
     this.seedPlantInstances = af.database.list('/plant-instances');
   }
