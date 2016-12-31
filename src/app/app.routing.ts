2f1b6496a25afbd9312e01b0cd2b38eb5450579e
Dawnut <dawnut.x@gmail.com>
2016-12-31 14:38:46 +0200

    Added new routings

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app-component/app.component';
+import { LoginComponent } from './login/login.component';
+import { PlantsWikiComponent } from './plants-wiki/plants-wiki.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserEditComponent } from './user-profile/user-edit/user-edit.component';
+import { UserGardenComponent } from './user-profile/user-garden/user-garden.component';
+import { UserPhotosComponent } from './user-profile/user-photos/user-photos.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/temp', pathMatch: 'full' },
-    { path: 'temp', component: AppComponent }  
-    // { path: 'plants', component: PlantsComponent },
-    // { path: 'details/:name', component: UsersComponent }
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: AppComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'plants', component: PlantsWikiComponent },
+    { path: 'user', component: UserProfileComponent },
+    { path: 'user/edit', component: UserEditComponent },
+    { path: 'user/garden', component: UserGardenComponent },
+    { path: 'user/photos', component: UserPhotosComponent },
 ];
 
 @NgModule({
