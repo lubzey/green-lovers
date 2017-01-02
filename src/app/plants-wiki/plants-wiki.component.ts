6c65875d705173b08765638dc89ef9611d37eb57
lubzey <lubzdudz@gmail.com>
2017-01-02 22:05:37 +0200

    Plants component improvements

--- src/app/plants-wiki/plants-wiki.component.ts
+++ src/app/plants-wiki/plants-wiki.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { PlantInstancesService } from '../shared/services/plant-instances.service';
+import { PlantInstance } from '../shared/models/plant-instance.model';
+
 
 @Component({
   selector: 'app-plants-wiki',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [require('./plants-wiki.component.css')]
 })
 export class PlantsWikiComponent implements OnInit {
+  plantInstances: Observable<PlantInstance[]>;
+  filteredPlantInstances: Observable<PlantInstance[]>;
 
-  constructor() { }
+  constructor(private plantInstancesService: PlantInstancesService) { }
 
   ngOnInit() {
+    this.getPlants();
   }
 
+  getPlants() {
+    this.plantInstances = this.plantInstancesService.getPlants();
+  }
 }
